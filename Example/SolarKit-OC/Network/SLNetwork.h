7afb1af1b3df778247ab17c425338f9336430257
wyhazq <wyhazq@gmail.com>
2017-07-05 17:16:40 +0800

    network

---
+++ Example/SolarKit-OC/Network/SLNetwork.h
@@ -0,0 +1,30 @@
+//
+//  SLNetwork.h
+//  Example
+//
+//  Created by wyh on 2017/7/5.
+//  Copyright © 2017年 SolarKit. All rights reserved.
+//
+
+
+//  0.基本配置 session配置
+//  1.请求和请求配置
+//  2.上传，断点续传
+//  3.下载，断点下载
+//  3.组合 completionGroup？
+//  4.请求时和回调时的多线程控制 completionQueue
+//  5.监测网络
+//  6.全局上传下载回调
+//  7.上传下载队列以及网络变换时的并发数处理
+//  8.开放请求配置：Serializer，HTTPS
+//  9.插件：把HUD回调到外面处理
+//  9.插件：把Request的加密解耦
+//  10.同步队列
+//  11.配置response正确错误的key和value
+//  12.请求参数的判断 断言NSAssert()NSParameterAssert()
+
+#ifndef SLNetwork_h
+#define SLNetwork_h
+
+
+#endif /* SLNetwork_h */
