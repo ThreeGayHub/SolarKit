89f2ce9bad0058ce53ae610e55866f19edd41814
wyhazq <wyhazq@gmail.com>
2017-07-05 11:31:41 +0800

    add example

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by wyh on 2017/7/5.
+//  Copyright © 2017年 SolarKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by wyh on 2017/7/5.
+//  Copyright © 2017年 SolarKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
