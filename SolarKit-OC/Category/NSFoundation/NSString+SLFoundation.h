7eae3e2ed2e1d1bdd44153f893cc61ff727602de
wyhazq <wyhazq@gmail.com>
2017-08-04 16:52:49 +0800

    NSString

--- SolarKit-OC/Category/NSFoundation/NSString+SLFoundation.h
+++ SolarKit-OC/Category/NSFoundation/NSString+SLFoundation.h
@@ -10,4 +10,44 @@
 
 @interface NSString (SLFoundation)
 
+NSString *ToString(id obj);
+
+//String -> Data
+@property (nonatomic, readonly) NSData *data;
+
+//String -> Base64Data
+@property (nonatomic, readonly) NSData *enBase64Data;
+
+//Base64String -> Data
+@property (nonatomic, readonly) NSData *deBase64Data;
+
+//String -> Base64String
+@property (nonatomic, readonly) NSString *enBase64String;
+
+//Base64String -> String
+@property (nonatomic, readonly) NSString *deBase64String;
+
+//Base64String -> Class
+@property (nonatomic, readonly) Class clazz;
+
+@property (nonatomic, readonly) NSString *localizedString;
+
+@property (nonatomic, readonly) NSString *noSpacingString;
+
+@property (nonatomic, readonly) BOOL isContainNumber;
+
+@property (nonatomic, readonly) BOOL isContainUpperLetters;
+
+@property (nonatomic, readonly) BOOL isContainLowerLetters;
+
+@property (nonatomic, readonly) BOOL isContainSpecialCharacters;
+
+@property (nonatomic, readonly) BOOL isContainChinese;
+
+//yyyy-MM-dd
+@property (nonatomic, readonly) NSDate *date;
+
+//yyyy-MM-dd HH:mm:ss
+@property (nonatomic, readonly) NSDate *dateTime;
+
 @end
--- SolarKit-OC/Category/NSFoundation/NSString+SLFoundation.m
+++ SolarKit-OC/Category/NSFoundation/NSString+SLFoundation.m
@@ -7,7 +7,141 @@
 //
 
 #import "NSString+SLFoundation.h"
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
 
 @implementation NSString (SLFoundation)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        
+        SEL originalSelector = @selector(containsString:);
+        SEL swizzledSelector = @selector(sl_containsString:);
+        
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+        if (success) {
+            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        }
+        else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+
+- (BOOL)sl_containsString:(NSString *)str {
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
+        return [self sl_containsString:str];
+    }
+    return [self rangeOfString:str].location != NSNotFound;
+}
+
+NSString *ToString(id obj) {
+    return (obj && ![obj isKindOfClass:[NSNull class]]) ? [NSString stringWithFormat:@"%@", obj] : @"";
+}
+
+- (NSData *)data {
+    return [self dataUsingEncoding:NSUTF8StringEncoding];
+}
+
+- (NSData *)enBase64Data {
+    return [self.data base64EncodedDataWithOptions:NSDataBase64Encoding64CharacterLineLength];
+}
+
+- (NSData *)deBase64Data {
+    return [[NSData alloc] initWithBase64EncodedString:self options:NSDataBase64DecodingIgnoreUnknownCharacters];
+}
+
+- (NSString *)enBase64String {
+    return [self.data base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+}
+
+- (NSString *)deBase64String {
+    return [[NSString alloc] initWithData:self.deBase64Data encoding:NSUTF8StringEncoding];
+}
+
+- (Class)clazz {
+    return NSClassFromString(self);
+}
+
+- (NSString *)localizedString {
+    return NSLocalizedString(self, nil);
+}
+
+- (NSString *)noSpacingString {
+    return [[self stringByReplacingOccurrencesOfString:@" " withString:@""] stringByReplacingOccurrencesOfString:@"　" withString:@""];
+}
+
+- (BOOL)isContainNumber {
+    BOOL result = NO;
+    if (self.length > 0) {
+        NSString *regex = @".*?\\d+.*?";
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+        result = [predicate evaluateWithObject:self];
+    }
+    return result;
+}
+
+- (BOOL)isContainUpperLetters {
+    BOOL result = NO;
+    if (self.length > 0) {
+        NSString *regex = @".*?[A-Z]+.*?";
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+        result = [predicate evaluateWithObject:self];
+    }
+    return result;
+}
+
+- (BOOL)isContainLowerLetters {
+    BOOL result = NO;
+    if (self.length > 0) {
+        NSString *regex = @".*?[a-z]+.*?";
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+        result = [predicate evaluateWithObject:self];
+    }
+    return result;
+}
+
+- (BOOL)isContainSpecialCharacters {
+    BOOL result = NO;
+    if (self.length > 0) {
+        NSString *regex = @".*?[^A-Za-z0-9]+.*?";
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+        result = [predicate evaluateWithObject:self];
+    }
+    return result;
+}
+
+- (BOOL)isContainChinese {
+    BOOL result = NO;
+    if (self.length > 0) {
+        NSString *regex = @"[\u4e00-\u9fa5]+";
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+        result = [predicate evaluateWithObject:self];
+    }
+    return result;
+}
+
+- (NSDate *)dateWithFormat:(NSString *)format {
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    formatter.dateFormat = format;
+    return [formatter dateFromString:self];
+}
+
+- (NSDate *)date {
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    formatter.dateFormat = @"yyyy-MM-dd";
+    return [formatter dateFromString:self];
+}
+
+- (NSDate *)dateTime {
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+    return [formatter dateFromString:self];
+}
 
 @end
